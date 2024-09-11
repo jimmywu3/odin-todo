1003c8114619702493d1bba38d832120d4b1f3e1
Jimmy Wu <jimmywu063@gmail.com>
2024-09-11 13:37:10 -0700

    Created resetPage function that will clear all children in the sidebar projects section and content section

--- src/domHelper.js
+++ src/domHelper.js
@@ -1,3 +1,15 @@
 const resetPage = () => {
-    
+    const sidebarProjects = document.querySelector(".project ul");
+    let child = sidebarProjects.lastElementChild;
+    while(child){
+        sidebarProjects.removeChild(child);
+        child = sidebarProjects.lastElementChild;
+    }
+
+    const content = document.querySelector(".content");
+    child = content.lastElementChild;
+    while(child){
+        content.removeChild(child);
+        child = contnet.lastElementChild;
+    } 
 }
